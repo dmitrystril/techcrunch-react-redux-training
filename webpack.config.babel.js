3ca5d57fd67f650f36463b2a56a22e399061ee83
Dmitry Stril <dmitry.stril@gmail.com>
2017-07-31 18:20:16 +0300

    updated webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,7 +1,7 @@
-import Config from 'webpack-config';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
+const Config = require("webpack-config");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 export default new Config().merge({
   entry: [
